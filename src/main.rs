db11bde5c42087255dd2f6f3dfd3fa7e33cefe08
Taher Mun <tahermun@gmail.com>
2021-01-14 17:30:43 -0500

    reduced size of buffer to store previous wavefronts; updated main

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,38 @@ mod wfa;
 mod tmp;
 
 fn main() {
-    let opts = wfa::WFAOpts {
-        a : 0,
-        x : 4,
-        o : 6,
-        e : 2,
-    };
-    let t = "GATACA";
-    let q = "GAGATA";
-    // let t = "GATACA";
-    // let q = "GATACA";
-    wfa::wfa::<i8>(opts, t.as_bytes(), q.as_bytes());
+    // tmp::tmp();
+    wfa_main();
+}
+
+fn wfa_main() {
+    let mut line = String::new(); 
+    match std::io::stdin().read_line(&mut line) {
+        Ok(_n) => {
+            // split string into two
+            let mut iter = line.split_whitespace();
+            let str1 = iter.next().unwrap_or("");
+            let str2 = iter.next().unwrap_or("");
+            if str1 == "" || str2 == "" {
+                std::process::exit(1);
+            } 
+            let opts = wfa::WFAOpts {
+                a: 0,
+                x: 4,
+                e: 2,
+                o: 6,
+            };
+            let score = wfa::wfa::<i8>(&opts, str1.as_bytes(), str2.as_bytes());
+            println!("{}", score);
+        }
+        Err(error) => {
+            println!("error: {}", error);
+        }
+    }
 }
 // 
-//    G A T A C A
-//  G 0 1 2 3 4 5
+//    g a t a c a
+//  g 0 1 2 3 4 5
 //  A 1 0 1 2 3 4
 //  G 2 1 0 1 2 3 
 //  A 3 2 1 0 1 2 
--- src/wfa.rs
+++ src/wfa.rs
@@ -1,5 +1,6 @@
 use num;
 use std::option::Option;
+use std::collections::VecDeque;
 
 #[cfg(target_arch = "x86_64")]
 use std::arch::x86_64::*;
@@ -24,10 +25,18 @@ impl Default for WFAOpts {
         }
     }
 }
+
+fn init_wf<T>(l: usize) -> Vec<T>
+where
+    T:  std::clone::Clone + num::FromPrimitive,
+{
+    return vec![T::from_i32(-1).unwrap(); l];
+}
+
 // T: integer. use larger ints for larger sequences
 // q.len() and t.len() must each be smaller than 2^sizeof(T)
 // https://doi.org/10.1093/bioinformatics/btaa777
-pub fn wfa<T>(opts: WFAOpts, t: &[u8], q: &[u8]) -> usize
+pub fn wfa<T>(opts: &WFAOpts, t: &[u8], q: &[u8]) -> usize
     where 
         T: num::PrimInt + num::Signed + num::Zero + num::One + num::FromPrimitive + num::traits::Saturating + std::clone::Clone + std::fmt::Debug,
 {
@@ -35,94 +44,107 @@ pub fn wfa<T>(opts: WFAOpts, t: &[u8], q: &[u8]) -> usize
     if q.len() > max_len || t.len() > max_len {
         panic!("query length {} or template length {} greater than max: {}. Please consider using a longer integer type", q.len(), t.len(), max_len);
     }
-    let k_max = t.len() + q.len() - 2;
-    let k_zero = q.len() - 1;
-    let a_k = match t.len() >= q.len() { // index of bottom right corner of DP matrix
+    let k_max : usize = t.len() + q.len() - 2;
+    let k_zero : usize = q.len() - 1;
+    let a_k : usize = match t.len() >= q.len() { // index of bottom right corner of DP matrix
         true => t.len() - q.len() + k_zero,
         false => q.len() - t.len() + k_zero,
     };
     let a_off : usize = t.len(); // furthest offset of longest diagonal
-    // TODO: how can we get around storing <max score> wavefronts?
-    let mut mwfs : Vec<Vec<T>> = vec![vec![T::from_i32(-1).unwrap(); k_max+1]; 256]; // match wavefront
-    mwfs[0][k_zero] = T::zero();
-    let mut iwfs : Vec<Vec<T>> = vec![vec![T::from_i32(-1).unwrap(); k_max+1]; 256]; // insertions
-    let mut dwfs : Vec<Vec<T>> = vec![vec![T::from_i32(-1).unwrap(); k_max+1]; 256]; // deletions
-    let mut mbounds : [[usize; 2]; 256] = [[k_zero; 2]; 256];
-    let mut ibounds : [[usize; 2]; 256] = [[k_zero + 1; 2]; 256];
-    let mut dbounds : [[usize; 2]; 256] = [[k_zero - 1; 2]; 256];
+    // current wavefronts
+    let mut wfs_s : [Vec<T>; 3] = [init_wf(k_max+1), init_wf(k_max+1), init_wf(k_max+1)]; // m, i, d at current score
+    wfs_s[0][k_zero] = T::zero();
+    // these are buffers of past WFs needed for each of the operations
+    let mut wfs_m : VecDeque<Vec<T>> = VecDeque::with_capacity(opts.x as usize);
+    let mut wfs_o : VecDeque<Vec<T>> = VecDeque::with_capacity((opts.o + opts.e) as usize);
+    let mut wfs_ie : VecDeque<Vec<T>> = VecDeque::with_capacity(opts.e as usize);
+    let mut wfs_de : VecDeque<Vec<T>> = VecDeque::with_capacity(opts.e as usize);
+    let mut bounds = [[k_zero, k_zero], [k_zero+1, k_zero+1], [k_zero-1, k_zero-1]];
     let mut s : usize = 0; // score (must be positive)
     loop {
         // wf_extend(m[s], q, t);
         // extend wavefront by searching down diagonals for matches
-        for k in mbounds[s][0]..mbounds[s][1] + 1 {
+        for k in bounds[0][0]..std::cmp::min(bounds[0][1], k_max) + 1 {
             if k > k_max {
                 break;
             }
-            let mut v = (mwfs[s][k].to_i32().unwrap() + (k_zero as i32 - k as i32)) as usize;
-            let mut h = mwfs[s][k].to_usize().unwrap_or(0);
+            let mut v : usize = (wfs_s[0][k].to_i32().unwrap() + (k_zero as i32 - k as i32)) as usize;
+            let mut h : usize = wfs_s[0][k].to_usize().unwrap_or(0);
             while v < q.len() && h < q.len() && q[v] == t[h] {
-                mwfs[s][k] = mwfs[s][k] + T::one();
+                wfs_s[0][k] = wfs_s[0][k] + T::one();
                 v = v + 1;
                 h = h + 1;
             }
         }
         // check to see if wavefront extended all the way to bottom right of DP matrix
-        if mwfs[s][a_k] >= T::from_usize(a_off).unwrap_or(T::zero()) {
+        if wfs_s[0][a_k] >= T::from_usize(a_off).unwrap_or(T::zero()) {
             break;
         }
-        // determine if we can start finding gaps
-        if s >= (opts.o + opts.e + opts.e) as usize {
-            s += *[opts.o + opts.e, opts.x, opts.e].iter().min().unwrap() as usize;
-        } else {
-            s += *[opts.o + opts.e, opts.x].iter().min().unwrap() as usize;
-        }
-        // TODO: return error if score goes too long
-        // can get rid of this once improvements are made to wavefront storage
-        if s > u8::MAX.into() {
-            panic!("scores over 255 are currently not supported (score = {})", s);
-        }
+        // accumulate wfs in buffers (one for each kind of score)
+        wfs_m.push_back(wfs_s[0].clone());
+        wfs_o.push_back(wfs_s[0].clone());
+        wfs_ie.push_back(wfs_s[1].clone());
+        wfs_de.push_back(wfs_s[2].clone());
+
+
+        // only increment by opts.e if gap open already happened
+        s += match s >= (opts.o + opts.e + opts.e) as usize {
+            true =>  *[opts.o + opts.e, opts.x, opts.e].iter().min().unwrap() as usize,
+            false => *[opts.o + opts.e, opts.x].iter().min().unwrap() as usize,
+        };
+
         // wf_next(m, i, d, q, t, s);
         // get bounds for new wavefront
         // wavefronts for [mismatch, ins, del]
-        let mut ixs : [Option<usize>; 3] = [None, None, None];
         // set bounds of M
+        let mut wf_m : Option<Vec<T>> = None;
+        let mut wf_o : Option<Vec<T>> = None;
+        let mut wf_ie : Option<Vec<T>> = None;
+        let mut wf_de : Option<Vec<T>> = None;
         if s >= opts.x as usize {
-            ixs[0] = Some(s - opts.x as usize);
-            mbounds[s][0] = mbounds[ixs[0].unwrap()][0] - 1;
-            mbounds[s][1] = mbounds[ixs[0].unwrap()][1] + 1;
+            if let Some(i) = wfs_m.pop_front() {
+                wf_m = Some(i);
+                bounds[0][0] = bounds[0][0] - 1;
+                bounds[0][1] = bounds[0][1] + 1;
+            };
         }
         if s >= (opts.o + opts.e) as usize {
-            ixs[1] = Some(s - (opts.o + opts.e) as usize);
+            wf_o =  wfs_o.pop_front() ;
         }
-        // Set bounds of I and D
+        // extend bounds of I and D only after one open+ext is done
         if s >= (opts.o + opts.e + opts.e) as usize { // when extension is possible
-            ixs[2] = Some(s - opts.e as usize);
-            ibounds[s][0] = ibounds[ixs[2].unwrap()][0] - 1;
-            dbounds[s][0] = dbounds[ixs[2].unwrap()][0] - 1;
-            ibounds[s][1] = ibounds[ixs[2].unwrap()][1] + 1;
-            dbounds[s][1] = dbounds[ixs[2].unwrap()][1] + 1;
+            if let Some(i) = wfs_ie.pop_front() {
+                wf_ie = Some(i); 
+                bounds[1][0] = bounds[1][0] - 1;
+                bounds[1][1] = bounds[1][1] + 1;
+            };
+            if let Some(i) = wfs_de.pop_front() {
+                wf_de = Some(i);
+                bounds[2][0] = bounds[2][0] - 1;
+                bounds[2][1] = bounds[2][1] + 1;
+            };
         }
         // update I, D, M here.
-        for k in ibounds[s][0]..ibounds[s][1]+1 {
-            iwfs[s][k] = match (ixs[1], ixs[2]) {
-                (Some(o), Some(e)) =>  std::cmp::max(mwfs[o][k-1], iwfs[e][k-1]) + T::one(),
-                (Some(o), None) =>  mwfs[o][k-1] + T::one(),
-                (None, Some(e)) =>  iwfs[e][k-1] + T::one(),
-                _ => iwfs[s][k],
+        for k in bounds[1][0]..bounds[1][1]+1 {
+            wfs_s[1][k] = match (&wf_o, &wf_ie) {
+                (Some(o), Some(e)) =>  std::cmp::max(o[k-1], e[k-1]) + T::one(),
+                (Some(o), None) =>  o[k-1] + T::one(),
+                (None, Some(e)) =>  e[k-1] + T::one(),
+                _ => wfs_s[1][k],
             }
         }
-        for k in dbounds[s][0]..dbounds[s][1]+1 {
-            dwfs[s][k] = match (ixs[1], ixs[2]) {
-                (Some(o), Some(e)) => std::cmp::max(mwfs[o][k+1], dwfs[e][k+1]),
-                (Some(o), None) => mwfs[o][k+1],
-                (None, Some(e)) => dwfs[e][k+1],
-                _ => dwfs[s][k],
+        for k in bounds[2][0]..bounds[2][1]+1 {
+            wfs_s[2][k] = match (&wf_o, &wf_de) {
+                (Some(o), Some(e)) => std::cmp::max(o[k+1], e[k+1]),
+                (Some(o), None) => o[k+1],
+                (None, Some(e)) => e[k+1],
+                _ => wfs_s[2][k],
             }
         }
-        for k in mbounds[s][0]..mbounds[s][1]+1 {
-            mwfs[s][k] = match ixs[0] {
-                Some(i) => *[mwfs[i][k]+T::one(), iwfs[s][k], dwfs[s][k]].iter().max().unwrap(),
-                _ => mwfs[s][k],
+        for k in bounds[0][0]..bounds[0][1]+1 {
+            wfs_s[0][k] = match &wf_m {
+                Some(m) => *[m[k]+T::one(), wfs_s[1][k], wfs_s[2][k]].iter().max().unwrap(),
+                _ => wfs_s[0][k],
             }
         }
     }
